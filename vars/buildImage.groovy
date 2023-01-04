8a62ce76c94b9acf883c9f4410e716b6594e83c3
Anas Mohamed <anasm0869@gmail.com>
2023-01-04 19:00:01 +0530

    docker image name(version) is paramiterized in groovy files

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/usr/bin/env groovy
 
-def call() {
+def call(String imageName) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t anasm98/javmav-app-anasm:jma-anasm-1.0.3 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push anasm98/javmav-app-anasm:jma-anasm-1.0.3'
+        sh "docker push $imageName"
     }
 }
