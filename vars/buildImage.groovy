ba2df266bc2142996beee86d23d542094a03a08d
Anas Mohamed <anasm0869@gmail.com>
2023-01-04 15:31:02 +0530

    docker image verios is updated

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -3,8 +3,8 @@
 def call() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-cred', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t anasm98/javmav-app-anasm:jma-anasm-1.0.2 .'
+        sh 'docker build -t anasm98/javmav-app-anasm:jma-anasm-1.0.3 .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push anasm98/javmav-app-anasm:jma-anasm-1.0.2'
+        sh 'docker push anasm98/javmav-app-anasm:jma-anasm-1.0.3'
     }
 }
